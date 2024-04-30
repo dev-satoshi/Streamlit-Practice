dbcb6b1f6bcc2a4d253a396cc159a763933fc7bf
dev-satoshi <dev.satoshi.1016@gmail.com>
2024-04-30 20:22:35 +0900

    feat: トップページを作成

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+
+# ページアイコンの設定
+# st.set_page_config(page_title="メインページ", page_icon="icon.png")
+
+# メインページ
+st.write("# Streamlit Practice")
+st.text("テキスト")
